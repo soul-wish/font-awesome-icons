88888f5b000dd10610ed8f751619efc70f0562d9
Sergey Lysenko <soulwish.ls@gmail.com>
2016-11-14 21:59:51 +0200

    add ability to check version of FA

--- index.js
+++ index.js
@@ -2,6 +2,17 @@ const got = require('got');
 const YAML = require('yamljs');
 const backupList = require('./data/icons.json');
 
-module.exports.getList = () => got('fontawesome.io/icons.yml')
+const getIcons = () => got('fontawesome.io/icons.yml')
     .then(response => YAML.parse(response.body.icons))
     .catch(() => backupList.icons);
+
+const printVersion = data => `${data[data.length - 1].created}.x`;
+
+module.exports.getList = getIcons;
+
+module.exports.version = () =>
+    new Promise((resolve, reject) => {
+        getIcons()
+            .then(data => resolve(printVersion(data)))
+            .catch(() => reject(printVersion(backupList.icons)));
+    });
