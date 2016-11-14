b60942a45ecb6faffaf9bf0700f423bde82f4c59
Sergey Lysenko <soulwish.ls@gmail.com>
2016-11-14 22:01:32 +0200

    add test for .version method

--- test.js
+++ test.js
@@ -11,3 +11,8 @@ test('Check if backup and latest FA icons are the same', async (t) => {
     const faIcons = await icons.getList();
     t.deepEqual(faIcons, iconsBackup.icons);
 });
+
+test('Check the version of FA', async (t) => {
+    const faVersion = await icons.version();
+    t.is(faVersion, '4.7.x');
+});
