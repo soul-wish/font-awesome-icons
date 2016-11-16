cdd4c75d7ab3b0b1417ab712980c849c935bb6cb
Sergey Lysenko <soulwish.ls@gmail.com>
2016-11-16 13:48:04 +0200

    add getCategories method

--- index.js
+++ index.js
@@ -8,11 +8,43 @@ const getIcons = () => got('fontawesome.io/icons.yml')
 
 const printVersion = data => `${data[data.length - 1].created}.x`;
 
+const getCategoriesObject = (data) => {
+    const categories = {};
+    data.forEach((icon) => {
+        icon.categories.forEach((category) => {
+            if (!{}.hasOwnProperty.call(categories, category)) {
+                categories[category] = [];
+            }
+            categories[category].push(icon);
+        });
+    });
+    return categories;
+};
+
+const getCategoriesArray = (data) => {
+    const categoriesObject = getCategoriesObject(data);
+    const categoriesArray = [];
+    Object.keys(categoriesObject).forEach((key) => {
+        categoriesArray.push({
+            name: key,
+            icons: categoriesObject[key],
+        });
+    });
+    return categoriesArray;
+};
+
 module.exports.getList = getIcons;
 
 module.exports.version = () =>
-    new Promise((resolve, reject) => {
+    new Promise((resolve) => {
         getIcons()
             .then(data => resolve(printVersion(data)))
-            .catch(() => reject(printVersion(backupList.icons)));
+            .catch(() => resolve(printVersion(backupList.icons)));
+    });
+
+module.exports.getCategories = () =>
+    new Promise((resolve) => {
+        getIcons()
+            .then(data => resolve(getCategoriesArray(data)))
+            .catch(() => resolve(getCategoriesArray(backupList.icons)));
     });
